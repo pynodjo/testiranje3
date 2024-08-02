ac56c4fadb47bae13fe6e6a319435ef6dd7e0f3d
pynodjo <pynodjo@gmail.com>
2024-08-03 01:21:24 +0200

    1

---
+++ app.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from flask import Flask, request, jsonify, render_template
+import json
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the JSON file
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+# Create dictionaries for quick lookup
+sifra_to_coordinates = {feature['properties']['SIFRA']: feature['geometry']['coordinates']
+                        for feature in data['features']
+                        if 'geometry' in feature and 'coordinates' in feature['geometry']}
+
+# Read and process the Excel file
+df = pd.read_excel('EP_Export_Uredjaja.xlsx', skiprows=6)
+df.columns = df.columns.str.strip()  # Trim blank spaces from column names
+
+# Remove duplicates
+df = df.drop_duplicates(subset=['Serijski', 'Šifra'])
+
+# Map 'Serijski' to 'Šifra' based on the Excel file
+serijski_broj_to_sifra = dict(zip(df['Serijski'], df['Šifra']))
+
+def find_coordinates_by_sifra(sifra, sifra_to_coordinates):
+    return sifra_to_coordinates.get(sifra, None)
+
+def find_sifra_by_serijski_broj(serijski_broj, serijski_broj_to_sifra):
+    return serijski_broj_to_sifra.get(serijski_broj, None)
+
+def create_google_maps_url(coordinates):
+    lon, lat = coordinates  # Reverse the order if needed
+    return f"https://www.google.com/maps?q={lat},{lon}"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_coordinates_by_sifra', methods=['POST'])
+def get_coordinates_by_sifra():
+    sifra = request.form.get('sifra')
+    if sifra:
+        try:
+            coordinates = find_coordinates_by_sifra(int(sifra), sifra_to_coordinates)
+            if coordinates:
+                url = create_google_maps_url(coordinates)
+                return jsonify({"url": url})
+        except ValueError:
+            return jsonify({"error": "Invalid SIFRA format."}), 400
+    return jsonify({"error": "SIFRA not found."}), 404
+
+@app.route('/get_coordinates_by_serijski_broj', methods=['POST'])
+def get_coordinates_by_serijski_broj():
+    serijski_broj = request.form.get('serijski_broj')
+    if serijski_broj:
+        sifra = find_sifra_by_serijski_broj(serijski_broj, serijski_broj_to_sifra)
+        if sifra:
+            coordinates = find_coordinates_by_sifra(sifra, sifra_to_coordinates)
+            if coordinates:
+                url = create_google_maps_url(coordinates)
+                return jsonify({"url": url})
+    return jsonify({"error": "SERIJSKI_BROJ not found."}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
